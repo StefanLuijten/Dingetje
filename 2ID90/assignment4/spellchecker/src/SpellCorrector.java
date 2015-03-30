3206c0c41954faaceb699adb506efc9bb836dace
StefanLuijten <sjaluijten@gmail.com>
2015-03-30 16:24:49 +0200

    getCandidateWords af en werkend.

--- 2ID90/assignment4/spellchecker/src/SpellCorrector.java
+++ 2ID90/assignment4/spellchecker/src/SpellCorrector.java
@@ -11,7 +11,6 @@ public class SpellCorrector {
     public SpellCorrector(CorpusReader cr, ConfusionMatrixReader cmr) {
         this.cr = cr;
         this.cmr = cmr;
-        getCandidateWords("test");
     }
 
     public String correctPhrase(String phrase) {
@@ -39,6 +38,16 @@ public class SpellCorrector {
     public HashSet<String> getCandidateWords(String word) {
         HashSet<String> ListOfWords = new HashSet<String>();
 
+        ListOfWords.addAll(getCandidateWordsDeletion(word));
+        ListOfWords.addAll(getCandidateWordsInsertion(word));
+        ListOfWords.addAll(getCandidateWordsSubstitution(word));
+        ListOfWords.addAll(getCandidateWordsTransposition(word));
+
+        return cr.inVocabulary(ListOfWords);
+    }
+
+    private HashSet<String> getCandidateWordsInsertion(String word) {
+        HashSet<String> ListOfWordsInsertion = new HashSet<String>();
         /*
          Try all insertions between each character and before/after the word.
          */
@@ -46,8 +55,8 @@ public class SpellCorrector {
         char[] charArray = word.toCharArray();
         // for all characters + after the word
         for (int i = 0; i < result.length; i++) {
-            //for the whole alpahbet
-            for (int j = 0; j < ALPHABET.length - 1; j++) {
+            //for the whole alphabet
+            for (int j = 0; j < ALPHABET.length; j++) {
                 // complete the word
                 for (int k = 0; k < result.length; k++) {
                     // index of the insertion
@@ -62,9 +71,79 @@ public class SpellCorrector {
                     }
                 }
                 String candidateWord = new String(result);
-                ListOfWords.add(candidateWord);
+                ListOfWordsInsertion.add(candidateWord);
             }
         }
-        return cr.inVocabulary(ListOfWords);
+        return ListOfWordsInsertion;
+    }
+
+    private HashSet<String> getCandidateWordsDeletion(String word) {
+        HashSet<String> ListOfWordsDeletion = new HashSet<String>();
+        /*
+         Try all deletions possible.
+         */
+        char[] result = new char[word.length() - 1];
+        char[] charArray = word.toCharArray();
+        // for all characters
+        for (int i = 0; i < charArray.length; i++) {
+            // complete the word
+            for (int k = 0; k < result.length + 1; k++) {
+                // index of the deletion
+                if (i == k) {
+                    // index already inserted correct the index.
+                } else if (i < k) {
+                    result[k - 1] = charArray[k];
+                    // no insertion yet. So no index correction.
+                } else {
+                    result[k] = charArray[k];
+                }
+            }
+            String candidateWord = new String(result);
+            ListOfWordsDeletion.add(candidateWord);
+        }
+        return ListOfWordsDeletion;
+    }
+
+    private HashSet<String> getCandidateWordsTransposition(String word) {
+        HashSet<String> ListOfWordsTransposition = new HashSet<String>();
+
+        char[] charArray = word.toCharArray();
+        char[] result = charArray.clone();
+        // for all characters
+        for (int i = 0; i < charArray.length; i++) {
+            char original = charArray[i];
+            // transpose with the n other chars in the word including the char itself.
+            for (int k = 0; k < charArray.length; k++) {
+                // clone the original word array;
+                result = charArray.clone();
+                // do all transpositons.
+                result[i] = charArray[k];
+                result[k] = original;
+                String candidateWord = new String(result);
+                ListOfWordsTransposition.add(candidateWord);
+            }
+        }
+        return ListOfWordsTransposition;
+    }
+
+    private HashSet<String> getCandidateWordsSubstitution(String word) {
+        HashSet<String> ListOfWordsSubstitution = new HashSet<String>();
+        /*
+         Try all subtitution for each character.
+         */
+        char[] charArray = word.toCharArray();
+        char[] result;
+        // for all characters
+        for (int i = 0; i < charArray.length; i++) {
+            //for the whole alphabet
+            for (int j = 0; j < ALPHABET.length; j++) {
+                result = charArray.clone();
+                result[i] = ALPHABET[j];
+
+                String candidateWord = new String(result);
+                ListOfWordsSubstitution.add(candidateWord);
+            }
+        }
+        return ListOfWordsSubstitution;
     }
 }
